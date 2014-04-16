822300c2ccde4262c3de0cc156ac6577e56f4ed5
Apollo Tang <tang.apollo@gmail.com>
2014-04-16 17:30:35 -0400

    add AppControl, router is up and working

---
+++ js/app/AppControl.js
@@ -0,0 +1,49 @@
+define(['main'],function(app){
+app.module("AppControl", function(AppControl, MyApp, Backbone, Marionette, $, _, Opts ) {
+	
+	console.log('in application control')
+	
+	var router = null;
+	this.Router = Marionette.AppRouter.extend({
+		appRoutes: {
+			  "items"         : 'itemsCallback'
+			, 'items/:id'     : 'itemsDetailCallback'
+			, 'items/:id/edit': 'itemEdit' 
+		}
+	});
+	
+    // Router callback
+	var _API = {
+		// the following callback ask View to to things:
+		  itemsCallback:       function()   { /*app.CollView.Controller.display();   */ console.log('itemsCallback fired ')}
+		, itemsDetailCallback: function(id) { /*app.Details.Controller.display(id);  */ console.log('itemsDetailCallback fired ')}
+		, itemEdit:            function(id) { /*app.EditItem.Controller.display(id); */ console.log('itemEdit fired ')}   
+	};
+	
+	app.addInitializer(function(){  /*  */
+		console.log( 'exec [2] application started');
+		new AppControl.Router({controller: _API	});
+	});
+	
+	//subscribe to app.trigger('AppControl:content:items');
+	// responsible: display collection
+	app.on("AppControl:content:items", function(){ 
+		app.navigate('items');
+		_API.itemsCallback();  // evoke router call back
+	});
+	
+	// subscribe to app.trigger('AppControl:content:itemById');
+	// responsible: display item by id
+	app.on("AppControl:content:itemById", function(id){ 
+		console.log('in AppControl:content:itemById ');
+		app.navigate('items/'+ id);
+		_API.itemsDetailCallback(id);  // evoke router callback
+	});
+	
+	app.on("AppControl:content:itemEdit", function(id){ 
+		app.navigate('items/'+ id + "/edit");
+		_API.itemEdit(id);
+	});	
+
+}, null);
+});
--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 define(["marionette"], function(Marionette){
+	
 	var app = new Marionette.Application();
-		
-	require(["app/MyModule"], function(myModule) {
-      app.MyModule.method2();         //hello from MyModule
-	  myModule.method2();            //hello from MyModule
-	  console.log('data2: ', myModule.data2);   //public data
-	  myModule.method3();            // {"a":"a","b":"b"}     
-	   
+	
+    app.addRegions({r1: "#main-region"});
+	app.addRegions({r2: "#dialog-region"});
+	
+	app.persisted = true;
+	app.useLocalStorage = true;
+
+	app.on("initialize:after", function(){ 
+		console.log( 'exec [3] after initializer');
+		if(Backbone.history){ 
+			console.log('backbone history is up');
+			Backbone.history.start(); 
+			if(app.getCurrentRoute() === ""){	
+				app.trigger('AppControl:content:items');
+			};
+		};
+	});
+
+	
+	//helper
+	_.extend(app, {
+		navigate: function(route, opts) { opts || (opts={}); Backbone.history.navigate(route, opts); }
+		, getCurrentRoute : function(){ return Backbone.history.fragment; }
 	});
 	
+	// start application control
+	require(['app/AppControl'], function(){});
+		
 	return app;
 });
