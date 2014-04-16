5867a4d5a7e374a2fa777053445898afba2891c7
Apollo Tang <tang.apollo@gmail.com>
2014-04-16 13:57:57 -0400

    initial commit for marionette file structure with AMD

---
+++ js/require_main.js
@@ -0,0 +1,31 @@
+requirejs.config({
+    baseUrl: "js"
+  , paths: {
+      backbone: "libs/backbone"
+    , jquery: "libs/jquery"
+    , json2: "libs/json2"
+    , localstorage: "libs/backbone.localstorage"
+    , marionette: "libs/backbone.marionette"
+    , text: "libs/text"
+    , tpl: "libs/underscore-tpl"
+    , underscore: "libs/underscore"
+  } //end path
+  , shim: {
+      underscore: { exports: "_" }
+    , backbone    : {
+      	  deps    : ["jquery", "underscore", "json2"]
+      	, exports : "Backbone"
+      }
+    , marionette  : {
+        deps      : ["backbone"]
+      , exports   : "Marionette"
+    }
+    , "jquery-ui"  : ["jquery"]
+    , localstorage : ["backbone"]
+    , tpl          : ["text"]
+  } // end shim
+}); // end requirejs.config
+
+require(["main"], function(app, tpl){
+  app.start();
+});
